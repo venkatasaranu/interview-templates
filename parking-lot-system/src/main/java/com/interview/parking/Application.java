b9054c470c4223bfde0b294e856e4ae26aad1b96
Venkata Saranu <Venkata.Saranu@aa.com>
2026-04-01 05:21:31 -0500

    parking

--- parking-lot-system/src/main/java/com/interview/parking/Application.java
+++ parking-lot-system/src/main/java/com/interview/parking/Application.java
@@ -1,4 +1,4 @@
-package com.aa.recipe.finder;
+package com.interview.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
